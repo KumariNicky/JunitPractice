eab873eec7d8eb0c0c275bda1953f338f7b820e8
Kumari Nicky <112064484+KumariNicky@users.noreply.github.com>
2023-02-22 09:15:57 +0530

    Update add.java

--- src/main/java/add.java
+++ src/main/java/add.java
@@ -1,1 +1,22 @@
-public class add {}
+public class Add {
+  int a;
+  int b;
+
+  public Add(int a, int b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  public int sum() {
+    int sum = 0;
+    try {
+      sum = a + b;
+      System.out.println(sum);
+    } catch (OutOfMemoryError e) {
+      System.out.println("Integer has crossed it's range");
+    } catch (Exception e) {
+      System.out.println("Something went wrong");
+    }
+    return sum;
+  }
+}
